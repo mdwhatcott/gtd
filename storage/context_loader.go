309339451aa86ea857822a972440baafde490f43
Michael Whatcott <mdwhatcott@gmail.com>
2018-02-15 16:05:19 -0700

    Not now

--- storage/context_loader.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-type ContextLoader interface {
-	AppendToContexts(task *Task)
-	FinishedTasks() ([]*Task, error)
-	Save(context *Context) error
-}
--- storage/project_loader.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-type ProjectLoader interface {
-	List() (names []string)
-	Load(name string) (*Project, error)
-	LoadFromTaskChecksum(checksum string) (*Project, error)
-	Save(project *Project) error
-	Delete(name string) error
-}
