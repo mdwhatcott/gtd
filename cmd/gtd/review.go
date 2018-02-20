c0db8a77410198dd6236c52e8a01951b2f39fd87
Michael Whatcott <mdwhatcott@gmail.com>
2018-02-20 06:11:18 -0700

    It's not necessary to commit so often.

--- cmd/gtd/review.go
+++ cmd/gtd/review.go
@@ -10,24 +10,22 @@ import (
 func reviewCLI(inputs []string) {
 	external.Flags(usageFlagsReview).Parse(inputs)
 
-	fmt.Println("Step 0/4: Commit all changes (this will happen serveral more times along the way).")
+	fmt.Println("Step 0/4: Commit and push all changes now (sourcetree window will open shortly).")
 	external.Commit(gtd.FolderRoot)
 
 	fmt.Println("Step 1/4: Process inbox, creating new projects as needed.")
 	createManyProjects()
-	external.Commit(gtd.FolderRoot)
 
 	fmt.Println("Step 2/4: Regenerate all task lists.")
 	syncTasks()
-	external.Commit(gtd.FolderRoot)
 
 	fmt.Println("Step 3/4: Review each project's content, tasks, and status.")
 	listProjects(gtd.LoadProjects(), true)
-	external.Commit(gtd.FolderRoot)
 
 	fmt.Println("Step 4/4: Regenerate all task lists.")
 	regenerateTasks()
+
+	fmt.Println("Review procedure complete. Commit and push all changes now.")
 	external.Commit(gtd.FolderRoot)
 
-	fmt.Println("Review procedure complete.")
 }
