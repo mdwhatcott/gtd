8d68a438ed50faa688bf3b14bc2daca0f4f22f09
Michael Whatcott <mdwhatcott@gmail.com>
2018-06-12 14:49:21 -0600

    Support for using month names as a recurring frequency.

--- gtd/recurring.go
+++ gtd/recurring.go
@@ -5,17 +5,54 @@ import "time"
 type Recurring int
 
 const (
-	RecurringNever        Recurring = iota
+	RecurringNever Recurring = iota
 	RecurringMonthly
 	RecurringBimonthly
 	RecurringQuarterly
 	RecurringSemiannually
 	RecurringAnnually
 	RecurringBiennially
+
+	RecurringJanuary
+	RecurringFebruary
+	RecurringMarch
+	RecurringApril
+	RecurringMay
+	RecurringJune
+	RecurringJuly
+	RecurringAugust
+	RecurringSeptember
+	RecurringOctober
+	RecurringNovember
+	RecurringDecember
 )
 
 func (this Recurring) Next(now time.Time) time.Time {
 	switch this {
+	case RecurringJanuary:
+		fallthrough
+	case RecurringFebruary:
+		fallthrough
+	case RecurringMarch:
+		fallthrough
+	case RecurringApril:
+		fallthrough
+	case RecurringMay:
+		fallthrough
+	case RecurringJune:
+		fallthrough
+	case RecurringJuly:
+		fallthrough
+	case RecurringAugust:
+		fallthrough
+	case RecurringSeptember:
+		fallthrough
+	case RecurringOctober:
+		fallthrough
+	case RecurringNovember:
+		fallthrough
+	case RecurringDecember:
+		return nextYear(now, recurringMonths[this])
 	case RecurringBiennially:
 		return now.AddDate(2, 0, 0)
 	case RecurringAnnually:
@@ -31,6 +68,16 @@ func (this Recurring) Next(now time.Time) time.Time {
 	default:
 		return now.AddDate(0, 0, 0)
 	}
+}
+
+func nextYear(now time.Time, target time.Month) time.Time {
+	if now.Month() == target {
+		now = now.AddDate(1, 0, 0)
+	}
+	for now.Month() != target {
+		now = now.AddDate(0, 1, 0)
+	}
+	return now
 }
 
 func (this Recurring) String() string {
@@ -50,17 +97,56 @@ func RecurringFromString(raw string) Recurring {
 }
 
 var recurringMap = map[interface{}]interface{}{
-	"monthly":             RecurringMonthly,
-	"bimonthly":           RecurringBimonthly,
-	"quarterly":           RecurringQuarterly,
-	"semiannually":        RecurringSemiannually,
-	"annually":            RecurringAnnually,
-	"biennially":          RecurringBiennially,
-	RecurringNever:        "",
-	RecurringMonthly:      "monthly",
-	RecurringBimonthly:    "bimonthly",
-	RecurringQuarterly:    "quarterly",
-	RecurringSemiannually: "semiannually",
-	RecurringAnnually:     "annually",
-	RecurringBiennially:   "biennially",
+	"monthly":               RecurringMonthly,
+	"bimonthly":             RecurringBimonthly,
+	"quarterly":             RecurringQuarterly,
+	"semiannually":          RecurringSemiannually,
+	"annually":              RecurringAnnually,
+	"biennially":            RecurringBiennially,
+	time.January.String():   RecurringJanuary,
+	time.February.String():  RecurringFebruary,
+	time.March.String():     RecurringMarch,
+	time.April.String():     RecurringApril,
+	time.May.String():       RecurringMay,
+	time.June.String():      RecurringJune,
+	time.July.String():      RecurringJuly,
+	time.August.String():    RecurringAugust,
+	time.September.String(): RecurringSeptember,
+	time.October.String():   RecurringOctober,
+	time.November.String():  RecurringNovember,
+	time.December.String():  RecurringDecember,
+	RecurringNever:          "",
+	RecurringMonthly:        "monthly",
+	RecurringBimonthly:      "bimonthly",
+	RecurringQuarterly:      "quarterly",
+	RecurringSemiannually:   "semiannually",
+	RecurringAnnually:       "annually",
+	RecurringBiennially:     "biennially",
+	RecurringJanuary:        time.January.String(),
+	RecurringFebruary:       time.February.String(),
+	RecurringMarch:          time.March.String(),
+	RecurringApril:          time.April.String(),
+	RecurringMay:            time.May.String(),
+	RecurringJune:           time.June.String(),
+	RecurringJuly:           time.July.String(),
+	RecurringAugust:         time.August.String(),
+	RecurringSeptember:      time.September.String(),
+	RecurringOctober:        time.October.String(),
+	RecurringNovember:       time.November.String(),
+	RecurringDecember:       time.December.String(),
+}
+
+var recurringMonths = map[Recurring]time.Month{
+	RecurringJanuary:   time.January,
+	RecurringFebruary:  time.February,
+	RecurringMarch:     time.March,
+	RecurringApril:     time.April,
+	RecurringMay:       time.May,
+	RecurringJune:      time.June,
+	RecurringJuly:      time.July,
+	RecurringAugust:    time.August,
+	RecurringSeptember: time.September,
+	RecurringOctober:   time.October,
+	RecurringNovember:  time.November,
+	RecurringDecember:  time.December,
 }
--- gtd/recurring_test.go
+++ gtd/recurring_test.go
@@ -17,10 +17,6 @@ type RecurringFixture struct {
 	now time.Time
 }
 
-func (this *RecurringFixture) Setup() {
-
-}
-
 func (this *RecurringFixture) TestRecurringBothWays() {
 	this.So(RecurringFromString(""), should.Equal, RecurringNever)
 	this.So(RecurringFromString("asdf"), should.Equal, RecurringNever)
@@ -30,6 +26,18 @@ func (this *RecurringFixture) TestRecurringBothWays() {
 	this.So(RecurringFromString("semiannually"), should.Equal, RecurringSemiannually)
 	this.So(RecurringFromString("annually"), should.Equal, RecurringAnnually)
 	this.So(RecurringFromString("biennially"), should.Equal, RecurringBiennially)
+	this.So(RecurringFromString("January"), should.Equal, RecurringJanuary)
+	this.So(RecurringFromString("February"), should.Equal, RecurringFebruary)
+	this.So(RecurringFromString("March"), should.Equal, RecurringMarch)
+	this.So(RecurringFromString("April"), should.Equal, RecurringApril)
+	this.So(RecurringFromString("May"), should.Equal, RecurringMay)
+	this.So(RecurringFromString("June"), should.Equal, RecurringJune)
+	this.So(RecurringFromString("July"), should.Equal, RecurringJuly)
+	this.So(RecurringFromString("August"), should.Equal, RecurringAugust)
+	this.So(RecurringFromString("September"), should.Equal, RecurringSeptember)
+	this.So(RecurringFromString("October"), should.Equal, RecurringOctober)
+	this.So(RecurringFromString("November"), should.Equal, RecurringNovember)
+	this.So(RecurringFromString("December"), should.Equal, RecurringDecember)
 
 	this.So(RecurringNever.String(), should.Equal, "")
 	this.So(RecurringMonthly.String(), should.Equal, "monthly")
@@ -38,6 +46,18 @@ func (this *RecurringFixture) TestRecurringBothWays() {
 	this.So(RecurringSemiannually.String(), should.Equal, "semiannually")
 	this.So(RecurringAnnually.String(), should.Equal, "annually")
 	this.So(RecurringBiennially.String(), should.Equal, "biennially")
+	this.So(RecurringJanuary.String(), should.Equal, "January")
+	this.So(RecurringFebruary.String(), should.Equal, "February")
+	this.So(RecurringMarch.String(), should.Equal, "March")
+	this.So(RecurringApril.String(), should.Equal, "April")
+	this.So(RecurringMay.String(), should.Equal, "May")
+	this.So(RecurringJune.String(), should.Equal, "June")
+	this.So(RecurringJuly.String(), should.Equal, "July")
+	this.So(RecurringAugust.String(), should.Equal, "August")
+	this.So(RecurringSeptember.String(), should.Equal, "September")
+	this.So(RecurringOctober.String(), should.Equal, "October")
+	this.So(RecurringNovember.String(), should.Equal, "November")
+	this.So(RecurringDecember.String(), should.Equal, "December")
 }
 
 func (this *RecurringFixture) TestNext() {
@@ -49,6 +69,44 @@ func (this *RecurringFixture) TestNext() {
 	this.So(RecurringSemiannually.Next(this.now), should.Equal, date(2000, 7, 1))
 	this.So(RecurringAnnually.Next(this.now), should.Equal, date(2001, 1, 1))
 	this.So(RecurringBiennially.Next(this.now), should.Equal, date(2002, 1, 1))
+}
+
+func (this *RecurringFixture) TestNext_FixedYearly() {
+	this.So(RecurringJanuary.Next(date(2000, 1, 1)), should.Equal, date(2001, 1, 1))
+	this.So(RecurringJanuary.Next(date(2000, 2, 1)), should.Equal, date(2001, 1, 1))
+
+	this.So(RecurringFebruary.Next(date(2000, 1, 1)), should.Equal, date(2000, 2, 1))
+	this.So(RecurringFebruary.Next(date(2000, 2, 1)), should.Equal, date(2001, 2, 1))
+
+	this.So(RecurringMarch.Next(date(2000, 1, 1)), should.Equal, date(2000, 3, 1))
+	this.So(RecurringMarch.Next(date(2000, 3, 1)), should.Equal, date(2001, 3, 1))
+
+	this.So(RecurringApril.Next(date(2000, 1, 1)), should.Equal, date(2000, 4, 1))
+	this.So(RecurringApril.Next(date(2000, 4, 1)), should.Equal, date(2001, 4, 1))
+
+	this.So(RecurringMay.Next(date(2000, 1, 1)), should.Equal, date(2000, 5, 1))
+	this.So(RecurringMay.Next(date(2000, 5, 1)), should.Equal, date(2001, 5, 1))
+
+	this.So(RecurringJune.Next(date(2000, 1, 1)), should.Equal, date(2000, 6, 1))
+	this.So(RecurringJune.Next(date(2000, 6, 1)), should.Equal, date(2001, 6, 1))
+
+	this.So(RecurringJuly.Next(date(2000, 1, 1)), should.Equal, date(2000, 7, 1))
+	this.So(RecurringJuly.Next(date(2000, 7, 1)), should.Equal, date(2001, 7, 1))
+
+	this.So(RecurringAugust.Next(date(2000, 1, 1)), should.Equal, date(2000, 8, 1))
+	this.So(RecurringAugust.Next(date(2000, 8, 1)), should.Equal, date(2001, 8, 1))
+
+	this.So(RecurringSeptember.Next(date(2000, 1, 1)), should.Equal, date(2000, 9, 1))
+	this.So(RecurringSeptember.Next(date(2000, 9, 1)), should.Equal, date(2001, 9, 1))
+
+	this.So(RecurringOctober.Next(date(2000, 1, 1)), should.Equal, date(2000, 10, 1))
+	this.So(RecurringOctober.Next(date(2000, 10, 1)), should.Equal, date(2001, 10, 1))
+
+	this.So(RecurringNovember.Next(date(2000, 1, 1)), should.Equal, date(2000, 11, 1))
+	this.So(RecurringNovember.Next(date(2000, 11, 1)), should.Equal, date(2001, 11, 1))
+
+	this.So(RecurringDecember.Next(date(2000, 1, 1)), should.Equal, date(2000, 12, 1))
+	this.So(RecurringDecember.Next(date(2000, 12, 1)), should.Equal, date(2001, 12, 1))
 }
 
 func date(year int, month time.Month, day int) time.Time {
