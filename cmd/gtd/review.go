6806c77988fcc1ce02e762b59810b36d33dd2eb1
Michael Whatcott <mdwhatcott@gmail.com>
2019-12-14 09:44:22 -0700

    Crossed off TODOs. Review text messages. Extracted and reordered methods.
    Also, no more checking google keep.

--- cmd/gtd/review.go
+++ cmd/gtd/review.go
@@ -20,44 +20,10 @@ func review() {
 
 func getClear() {
 	fmt.Println("Part 1: Get Clear")
-	commit()
 	mindSweep()
 	inboxZero()
-	calendar()
-}
-
-func getCurrent() {
-	fmt.Println("Part 2: Get Current")
-	reviewEmailTags()
-	importTickler()
-	generateTasks()
-	taskReview()
-	generateTasks()
-	projectReview()
-	generateTasks()
-	reviewProjectList()
 	commit()
 }
-
-func reviewEmailTags() {
-	external.Prompt("Review email tags (needs reply, read later, project support, waiting for response)." + enterToContinue)
-}
-
-func reviewProjectList() {
-	fmt.Println("Here is the projects list for one final review:")
-	listProjects()
-}
-
-func getCreative() {
-	fmt.Println("Part 3: Get Creative! (review someday/maybe, upper horizons of focus, etc...)")
-}
-
-func commit() {
-	fmt.Println("Commit current state. <ENTER> to continue...")
-	external.Commit(gtd.FolderRoot)
-}
-
-// TODO: promote this to a CLI action
 func mindSweep() {
 	const durationPrompt = "What has your attention? Enter duration of mind sweep (or <ENTER> to continue):"
 	answer := external.Prompt(durationPrompt)
@@ -73,46 +39,87 @@ func mindSweep() {
 		}
 	}
 }
-
-const enterToContinue = " <ENTER> to continue..."
-
-// TODO: promote this to a CLI action
 func inboxZero() {
 	fmt.Println(`Get "In" to Zero!`)
-	external.Prompt("1. Clear desk, moving 'stuff' to the in-tray." + enterToContinue)
-	external.Prompt("2. Unload stuff from messenger bag to the in-tray." + enterToContinue)
-	external.Prompt("3. Process in-tray." + enterToContinue)
-	createProjects()
-	external.Prompt("4. Process ~/Desktop." + enterToContinue)
-	createProjects()
-	external.Prompt("5. Process Google Keep." + enterToContinue)
-	createProjects()
-	external.Prompt("6. Process e-mail." + enterToContinue)
-	createProjects()
+	clearDesk()
+	unloadMessengerBag()
+	processInTray()
+	processDesktop()
+	processEmail()
 	external.Prompt("All in-boxes should be empty at this point. <ENTER> to continue...")
 }
+func clearDesk() string {
+	return external.Prompt("Clear the desk, moving 'stuff' to the in-tray." + enterToContinue)
+}
+func unloadMessengerBag() string {
+	return external.Prompt("Unload stuff from messenger bag to the in-tray." + enterToContinue)
+}
+func processInTray() {
+	external.Prompt("Process the in-tray." + enterToContinue)
+	createProjects()
+}
+func processDesktop() {
+	external.Prompt("Process ~/Desktop." + enterToContinue)
+	createProjects()
+}
+func processEmail() {
+	external.Prompt("Process e-mail." + enterToContinue)
+	createProjects()
+}
 
-func calendar() {
+func getCurrent() {
+	fmt.Println("Part 2: Get Current")
+	reviewTextMessages()
+	reviewEmailTags()
+	reviewCalendar()
+	importTickler()
+	taskReview()
+	projectReview()
+	reviewProjectList()
+	commit()
+}
+func reviewTextMessages() {
+	external.Prompt("Review recent text messages." + enterToContinue)
+	createProjects()
+}
+func reviewEmailTags() {
+	external.Prompt("Review email tags (needs reply, read later, project support, waiting for response)." + enterToContinue)
+}
+func reviewCalendar() {
 	external.Prompt("Review previous and upcoming 2 weeks in calendar." + enterToContinue)
 	createProjects()
 }
-
 func importTickler() {
 	fmt.Println("Scanning tickler for projects now due...")
 	scanTickler()
+	generateTasks()
 }
-
+func taskReview() {
+	fmt.Println("Review task lists in sequence...")
+	reviewTasks()
+	generateTasks()
+}
+func projectReview() {
+	fmt.Println("Review project status in sequence.")
+	reviewProjects()
+	generateTasks()
+}
+func reviewProjectList() {
+	fmt.Println("Here is the projects list for one final review:")
+	listProjects()
+}
 func generateTasks() {
 	fmt.Println("Regenerating task lists...")
 	regenerateTasks()
 }
 
-func taskReview() {
-	fmt.Println("Review task lists in sequence...")
-	reviewTasks()
+func getCreative() {
+	fmt.Println("Part 3: Get Creative! ('clean a drawer', review someday/maybe, review upper horizons of focus, etc...)")
 }
 
-func projectReview() {
-	fmt.Println("Review project status in sequence.")
-	reviewProjects()
+func commit() {
+	fmt.Println("Commit current state. <ENTER> to continue...")
+	external.Commit(gtd.FolderRoot)
 }
+
+const enterToContinue = " <ENTER> to continue..."
