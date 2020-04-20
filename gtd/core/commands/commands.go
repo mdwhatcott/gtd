e22068d17fac2fe5a7e1b7864392c6bd54ce2f48
Michael Whatcott <mdwhatcott@gmail.com>
2020-04-19 21:23:34 -0600

    Removed unnecessary line breaks.

--- gtd/core/commands/commands.go
+++ gtd/core/commands/commands.go
@@ -1,133 +1,96 @@
 package commands
 
 type TrackOutcome struct {
-	Title string
-
+	Title  string
 	Result CreateResult
 }
-
 type UpdateOutcomeTitle struct {
 	OutcomeID string
 	NewTitle  string
-
-	Result Result
+	Result    Result
 }
-
 type UpdateOutcomeExplanation struct {
 	OutcomeID   string
 	Explanation string
-
-	Result Result
+	Result      Result
 }
-
 type UpdateOutcomeDescription struct {
 	OutcomeID          string
 	UpdatedDescription string
-
-	Result Result
+	Result             Result
 }
-
 type DeleteOutcome struct {
 	OutcomeID string
-
-	Result Result
+	Result    Result
 }
-
 type DeclareOutcomeFixed struct {
 	OutcomeID string
-
-	Result Result
+	Result    Result
 }
-
 type DeclareOutcomeRealized struct {
 	OutcomeID string
-
-	Result Result
+	Result    Result
 }
-
 type DeclareOutcomeAbandoned struct {
 	OutcomeID string
 	Reason    string
-
-	Result Result
+	Result    Result
 }
-
 type DeclareOutcomeDeferred struct {
 	OutcomeID string
-
-	Result Result
+	Result    Result
 }
-
 type DeclareOutcomeUncertain struct {
 	OutcomeID string
-
-	Result Result
+	Result    Result
 }
-
 type TrackAction struct {
 	OutcomeID   string
 	Description string
-
-	Result struct {
+	Result      struct {
 		Error    error
 		ActionID string
 	}
 }
-
 type ReorderAction struct {
 	OutcomeID string
 	ActionID  string
 	NewOrder  float64
-
-	Result Result
+	Result    Result
 }
-
 type UpdateActionDescription struct {
 	OutcomeID      string
 	ActionID       string
 	NewDescription string
-
-	Result Result
+	Result         Result
 }
-
 type MarkActionStatusLatent struct {
 	OutcomeID string
 	ActionID  string
-
-	Result Result
+	Result    Result
 }
-
 type MarkActionStatusIncomplete struct {
 	OutcomeID string
 	ActionID  string
-
-	Result Result
+	Result    Result
 }
-
 type MarkActionStatusComplete struct {
 	OutcomeID string
 	ActionID  string
-
-	Result Result
+	Result    Result
 }
-
 type MarkActionStrategySequential struct {
 	OutcomeID string
 	ActionID  string
-
-	Result Result
+	Result    Result
 }
-
 type MarkActionStrategyConcurrent struct {
 	OutcomeID string
 	ActionID  string
-
-	Result Result
+	Result    Result
 }
-
 type DeleteAction struct {
 	OutcomeID string
 	ActionID  string
-
-	Result Result
+	Result    Result
 }
--- gtd/core/events/events.go
+++ gtd/core/events/events.go
@@ -5,56 +5,46 @@ type OutcomeTrackedV1 struct {
 	OutcomeID string `json:"outcome_id"`
 	Title     string `json:"definition"`
 }
-
 type OutcomeTitleUpdatedV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	NewTitle  string `json:"new_title"`
 }
-
 type OutcomeExplanationUpdatedV1 struct {
 	Timestamp   Time   `json:"timestamp"`
 	OutcomeID   string `json:"outcome_id"`
 	Explanation string `json:"explanation"`
 }
-
 type OutcomeDescriptionUpdatedV1 struct {
 	Timestamp      Time   `json:"timestamp"`
 	OutcomeID      string `json:"outcome_id"`
 	NewDescription string `json:"description"`
 }
-
 type OutcomeDeletedV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 }
-
 type OutcomeFixedV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 }
-
 type OutcomeRealizedV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 }
-
 type OutcomeAbandonedV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	Reason    string `json:"reason"`
 }
-
 type OutcomeDeferredV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 }
-
 type OutcomeUncertainV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 }
-
 type ActionTrackedV1 struct {
 	Timestamp   Time     `json:"timestamp"`
 	OutcomeID   string   `json:"outcome_id"`
@@ -64,51 +54,43 @@ type ActionTrackedV1 struct {
 	IsComplete  bool     `json:"is_complete"`
 	Sequence    float64  `json:"sequence"`
 }
-
 type ActionReorderedV1 struct {
 	Timestamp   Time    `json:"timestamp"`
 	OutcomeID   string  `json:"outcome_id"`
 	ActionID    string  `json:"action_id"`
 	NewSequence float64 `json:"new_definition"`
 }
-
 type ActionDescriptionUpdatedV1 struct {
 	Timestamp     Time   `json:"timestamp"`
 	OutcomeID     string `json:"outcome_id"`
 	ActionID      string `json:"action_id"`
 	NewDefinition string `json:"new_definition"`
 }
-
 type ActionStatusMarkedLatentV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	ActionID  string `json:"action_id"`
 }
-
 type ActionStatusMarkedIncompleteV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	ActionID  string `json:"action_id"`
 }
-
 type ActionStatusMarkedCompleteV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	ActionID  string `json:"action_id"`
 }
-
 type ActionStrategyMarkedSequentialV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	ActionID  string `json:"action_id"`
 }
-
 type ActionStrategyMarkedConcurrentV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
 	ActionID  string `json:"action_id"`
 }
-
 type ActionDeletedV1 struct {
 	Timestamp Time   `json:"timestamp"`
 	OutcomeID string `json:"outcome_id"`
