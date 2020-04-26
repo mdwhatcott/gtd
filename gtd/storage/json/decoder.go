9b8ce5baf65413a33a7f1627f75c277fca2c3d54
Michael Whatcott <mdwhatcott@gmail.com>
2020-04-25 23:03:12 -0600

    Rewrote the JSON decoder. It now streams all items to a channel.

--- gtd/storage/json/decoder.go
+++ gtd/storage/json/decoder.go
@@ -2,24 +2,63 @@ package json
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"reflect"
 )
 
 type Decoder struct {
 	inner *json.Decoder
+	err   error
 }
 
 func NewDecoder(reader io.Reader) *Decoder {
 	return &Decoder{inner: json.NewDecoder(reader)}
 }
 
-func (this *Decoder) DecodeType(registry map[string]reflect.Type) interface{} {
-	var type_ string
-	_ = this.inner.Decode(&type_)
-	return reflect.New(registry[type_]).Elem()
+func (this *Decoder) Error() error {
+	return this.err
 }
 
-func (this *Decoder) Decode(v interface{}) error {
-	return this.inner.Decode(&v)
+func (this *Decoder) DecodeAll(registry map[string]reflect.Type) chan interface{} {
+	all := make(chan interface{})
+	go this.decodeAll(registry, all)
+	return all
 }
+
+func (this *Decoder) decodeAll(registry map[string]reflect.Type, all chan interface{}) {
+	defer close(all)
+	for {
+		var value interface{}
+		value, this.err = this.decodeType(registry)
+		if this.err == io.EOF {
+			this.err = nil
+			break
+		}
+		if this.err != nil {
+			break
+		}
+
+		this.err = this.inner.Decode(&value)
+		if this.err != nil {
+			break
+		}
+		all <- value
+	}
+}
+
+func (this *Decoder) decodeType(registry map[string]reflect.Type) (interface{}, error) {
+	var name string
+	err := this.inner.Decode(&name)
+	if err == io.EOF {
+		return nil, err
+	}
+	type_ := registry[name]
+	if type_ == nil {
+		return nil, fmt.Errorf("%w: [%s]", errUnregisteredType, name)
+	}
+	return reflect.New(type_).Elem(), nil
+}
+
+var errUnregisteredType = errors.New("unregistered type name")
--- gtd/storage/json/decoder_test.go
+++ gtd/storage/json/decoder_test.go
@@ -2,6 +2,7 @@ package json
 
 import (
 	"bytes"
+	"io"
 	"reflect"
 	"testing"
 
@@ -16,25 +17,68 @@ func TestDecoderFixture(t *testing.T) {
 type DecoderFixture struct {
 	*gunit.Fixture
 
-	source  *bytes.Buffer
-	decoder *Decoder
+	source   *bytes.Buffer
+	decoder  *Decoder
+	registry map[string]reflect.Type
 }
 
 func (this *DecoderFixture) Setup() {
 	this.source = new(bytes.Buffer)
 	this.decoder = NewDecoder(this.source)
+	this.registry = map[string]reflect.Type{
+		"string": reflect.TypeOf(""),
+	}
 }
 
-func (this *DecoderFixture) Test() {
-	this.source.WriteString(`"int"` + "\n")
-	this.source.WriteString("42" + "\n")
-	registry := map[string]reflect.Type{
-		"int": reflect.TypeOf(42),
+func (this *DecoderFixture) TestDecodeAll() {
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"a"` + "\n")
+
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"b"` + "\n")
+
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"c"` + "\n")
+
+	all := this.decoder.DecodeAll(this.registry)
+
+	this.So(gather(all), should.Resemble, []interface{}{"a", "b", "c"})
+}
+
+func (this *DecoderFixture) TestDecodeAll_MissingValue() {
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"a"` + "\n")
+
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"b"` + "\n")
+
+	this.source.WriteString(`"string"` + "\n")
+	//this.source.WriteString(`"c"` + "\n") // missing last line!
+
+	all := this.decoder.DecodeAll(this.registry)
+
+	this.So(gather(all), should.Resemble, []interface{}{"a", "b"})
+	this.So(this.decoder.Error(), should.Wrap, io.EOF)
+}
+func (this *DecoderFixture) TestDecodeAll_UnregisteredType() {
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"a"` + "\n")
+
+	this.source.WriteString(`"string"` + "\n")
+	this.source.WriteString(`"b"` + "\n")
+
+	this.source.WriteString(`"UNREGISTERED-TYPE"` + "\n")
+	this.source.WriteString(`"c"` + "\n")
+
+	all := this.decoder.DecodeAll(this.registry)
+
+	this.So(gather(all), should.Resemble, []interface{}{"a", "b"})
+	this.So(this.decoder.Error(), should.Wrap, errUnregisteredType)
+	this.So(this.decoder.Error().Error(), should.ContainSubstring, "UNREGISTERED-TYPE")
+}
+func gather(queue chan interface{}) (all []interface{}) {
+	for item := range queue {
+		all = append(all, item)
 	}
-
-	value := this.decoder.DecodeType(registry)
-	err := this.decoder.Decode(&value)
-
-	this.So(err, should.BeNil)
-	this.So(value, should.Equal, 42)
+	return all
 }
