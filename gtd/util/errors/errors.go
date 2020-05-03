e9d948914c3230beed9bb09e483d7c9ddeb5d541
Michael Whatcott <mdwhatcott@gmail.com>
2020-05-02 21:11:39 -0600

    Include file:line in wrapped error value.

--- gtd/util/errors/errors.go
+++ gtd/util/errors/errors.go
@@ -3,10 +3,17 @@ package errors
 import (
 	"errors"
 	"fmt"
+	"runtime"
 )
 
-var New = errors.New
+var (
+	New    = errors.New
+	Is     = errors.Is
+	As     = errors.As
+	Unwrap = errors.Unwrap
+)
 
 func Wrap(outer error, inner interface{}) error {
-	return fmt.Errorf("%w: [%v]", outer, inner)
+	_, file, line, _ := runtime.Caller(1)
+	return fmt.Errorf("%w: [%v] (at %s:%d)", outer, inner, file, line)
 }
