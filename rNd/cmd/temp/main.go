999bb68b3821ebf5a2b0ee833234bd595a2c469e
Michael Whatcott <mdwhatcott@gmail.com>
2020-05-11 22:31:39 -0600

    Refactored.

--- rNd/cmd/temp/main.go
+++ rNd/cmd/temp/main.go
@@ -9,36 +9,47 @@ import (
 )
 
 func main() {
+	name := createTempFile()
+	fmt.Println(name)
+
+	all := editFile(name)
+	fmt.Println(all)
+
+	deleteFile(name)
+
+}
+
+func createTempFile() string {
 	file, err := ioutil.TempFile("", "")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(file.Name())
-
-	out, err := exec.Command("subl", file.Name(), "--wait").CombinedOutput()
-	if err != nil {
-		log.Fatal(err)
+	err2 := file.Close()
+	if err2 != nil {
+		log.Fatal(err2)
 	}
 
-	fmt.Println(string(out))
+	return file.Name()
+}
 
-	err = file.Close()
-	if err != nil {
-		log.Fatal(err)
+func editFile(name string) string {
+	_, err2 := exec.Command("subl", name, "--wait").CombinedOutput()
+	if err2 != nil {
+		log.Fatal(err2)
 	}
 
-	all, err := ioutil.ReadFile(file.Name())
-	if err != nil {
-		log.Fatal(err)
+	all, err3 := ioutil.ReadFile(name)
+	if err3 != nil {
+		log.Fatal(err3)
 	}
 
-	fmt.Println("Content:")
-	fmt.Println(string(all))
+	return string(all)
+}
 
-	err = os.Remove(file.Name())
-	if err != nil {
-		log.Fatal(err)
+func deleteFile(name string) {
+	err4 := os.Remove(name)
+	if err4 != nil {
+		log.Fatal(err4)
 	}
-
 }
