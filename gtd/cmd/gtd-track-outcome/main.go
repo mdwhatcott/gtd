06b569c7a41c3182049a7a61e437dbb66944596d
Michael Whatcott <mdwhatcott@gmail.com>
2020-05-30 16:02:16 -0600

    Storage wireup.

--- gtd/cmd/gtd-track-outcome/main.go
+++ gtd/cmd/gtd-track-outcome/main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/google/uuid"
 
-	"github.com/mdwhatcott/gtd/gtd/core/events"
-	"github.com/mdwhatcott/gtd/gtd/core/wireup"
-	"github.com/mdwhatcott/gtd/gtd/storage"
-	"github.com/mdwhatcott/gtd/gtd/storage/eventstore"
-	"github.com/mdwhatcott/gtd/gtd/storage/json"
-	"github.com/mdwhatcott/gtd/gtd/ui"
-	"github.com/mdwhatcott/gtd/gtd/ui/tempfile"
-	"github.com/mdwhatcott/gtd/gtd/ui/ux"
+	/*****/
+	core "github.com/mdwhatcott/gtd/gtd/core/wireup"
+	/**/ storage "github.com/mdwhatcott/gtd/gtd/storage/wireup"
+	/**********/ "github.com/mdwhatcott/gtd/gtd/ui"
+	/**********/ "github.com/mdwhatcott/gtd/gtd/ui/tempfile"
+	/**********/ "github.com/mdwhatcott/gtd/gtd/ui/ux"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	HANDLER := wireup.BuildOutcomesHandler(wireup.Requirements{
-		IDFunc: func() string { return uuid.New().String() },
-		Reader: eventstore.NewReader(reading, decoding),
-		Writer: eventstore.NewWriter(encoding, writing),
+	gtdPath, ok := os.LookupEnv("GTDPATH")
+	if !ok {
+		log.Fatal("The 'GTDPATH' environment variable required for resolution of event store file.")
+	}
+
+	PATH := filepath.Join(gtdPath, "events.json")
+	HANDLER := core.BuildOutcomesHandler(core.Requirements{
+		IDFunc: ID,
+		Reader: storage.BuildEventStoreReader(PATH),
+		Writer: storage.BuildEventStoreWriter(PATH),
 	})
 	CONTENT := tempfile.NewEditor().EditTempFile(ui.TrackOutcomeTemplate)
 	PARSER := ux.NewOutcomeDetailParser(HANDLER, "", nil, CONTENT)
@@ -34,19 +37,6 @@ func main() {
 	}
 }
 
-func decoding(_reader io.Reader) storage.Decoder { return json.NewDecoder(_reader, events.Registry()) }
-func encoding(_writer io.Writer) storage.Encoder { return json.NewEncoder(_writer) }
-
-func reading() io.ReadCloser  { return open("events.json") }
-func writing() io.WriteCloser { return open("events.json") }
-
-func open(_name string) *os.File {
-	PATH := filepath.Join(storagePath, _name)
-	FILE, ERR := os.OpenFile(PATH, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	if ERR != nil {
-		panic(ERR)
-	}
-	return FILE
+func ID() string {
+	return uuid.New().String()
 }
-
-var storagePath = filepath.Join(os.Getenv("HOME"), "Desktop")
---
+++ gtd/storage/wireup/wireup.go
@@ -0,0 +1,34 @@
+package wireup
+
+import (
+	"io"
+	"os"
+
+	"github.com/smartystreets/joyride/v2"
+
+	"github.com/mdwhatcott/gtd/gtd/core/events"
+	"github.com/mdwhatcott/gtd/gtd/storage"
+	"github.com/mdwhatcott/gtd/gtd/storage/eventstore"
+	"github.com/mdwhatcott/gtd/gtd/storage/json"
+)
+
+func BuildEventStoreReader(path string) joyride.StorageReader {
+	return eventstore.NewReader(reading(path), decoding)
+}
+func BuildEventStoreWriter(path string) joyride.StorageWriter {
+	return eventstore.NewWriter(encoding, writing(path))
+}
+
+func decoding(_reader io.Reader) storage.Decoder { return json.NewDecoder(_reader, events.Registry()) }
+func encoding(_writer io.Writer) storage.Encoder { return json.NewEncoder(_writer) }
+
+func reading(path string) func() io.ReadCloser  { return func() io.ReadCloser { return open(path) } }
+func writing(path string) func() io.WriteCloser { return func() io.WriteCloser { return open(path) } }
+
+func open(_path string) *os.File {
+	FILE, ERR := os.OpenFile(_path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if ERR != nil {
+		panic(ERR)
+	}
+	return FILE
+}
