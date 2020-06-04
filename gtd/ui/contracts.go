630e2b3a5657aaa2cf5c508fdec7c2720f948ee5
Michael Whatcott <mdwhatcott@gmail.com>
2020-06-04 09:12:02 -0600

    Removed dead template.

--- gtd/ui/contracts.go
+++ gtd/ui/contracts.go
@@ -3,16 +3,3 @@ package ui
 type Editor interface {
 	EditTempFile(initialContent string) (resultContent string)
 }
-
-const TrackOutcomeTemplate = `# {TITLE}
-
-> {EXPLANATION}
-
-
-## Actions:
-
--  [ ] concurrent @home
-1. [ ] sequential @home
-
-
-## Support Materials:`
